a0e9d5b13ec65164eac8eba228951946e0e43f6b
Woojgh <jamessalamonsen@gmail.com>
2017-04-26 10:44:29 -0700

    Got most of todods done

--- james-brandon/public/scripts/controllers/routes.js
+++ james-brandon/public/scripts/controllers/routes.js
@@ -1,15 +1,12 @@
 'use strict';
 
-let outString = '';
-
-page('/', () => {
-  outString = `
-    
-  `;
-  $('main').html(outString);
-});
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// :D:D:D:D TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+page('/', articleController.init);
+page('/new', newView.init);
+page('admin', adminView.init);
+page('/about', aboutController.init);
+
+// :D:D:D:D TODO: What function do you call to activate page.js? Fire it off now, to execute
 page();
